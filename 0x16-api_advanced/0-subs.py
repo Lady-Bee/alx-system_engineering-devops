3dbf36a6b884aa3c0319396711f8426c531c0315
Lady-Bee <bisimi16@gmail.com>
2024-06-05 09:11:04 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,8 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'user-agent': 'egsyquest'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
